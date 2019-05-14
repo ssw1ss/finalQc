08dfa7f163692e36f19e168cabee675d54eab115
Lukas Hostettler <Lukas@mac.home>
2019-05-14 14:32:04 -0400

    sure

--- src/templates/products/index.js
+++ src/templates/products/index.js
@@ -1,31 +1,45 @@
 import React from "react"
-import { ApolloClient } from "apollo-client"
-import { createHttpLink } from "apollo-link-http"
-import { InMemoryCache } from "apollo-cache-inmemory"
-import { ApolloProvider } from "react-apollo"
+// import { ApolloClient } from "apollo-client"
+// import { createHttpLink } from "apollo-link-http"
+// import { InMemoryCache } from "apollo-cache-inmemory"
+// import { ApolloProvider } from "react-apollo"
 import { graphql } from "gatsby"
+import Link from "gatsby-link"
+import Img from "gatsby-image"
 
-import Client from "./Client"
+// import Client from "./Client"
 
-const client = new ApolloClient({
-  link: createHttpLink({
-    uri: "http://localhost:8000/___graphql",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    }
-  }),
-  cache: new InMemoryCache()
-})
+// const client = new ApolloClient({
+//   link: createHttpLink({
+//     uri: "http://localhost:8000/___graphql",
+//     headers: {
+//       Accept: "application/json",
+//       "Content-Type": "application/json"
+//     }
+//   }),
+//   cache: new InMemoryCache()
+// })
 
 const ProductsPage = ({ data }) => {
   return (
     <div>
-      <ApolloProvider client={client}>
-        <div style={{ padding: "100px 0" }}>
-          <Client data={data} client={client} />
-        </div>
-      </ApolloProvider>
+      <div style={{ background: "#ddd" }}>
+        {data.allMdx.edges.map(({ node }) => {
+          const { frontmatter, fields, excerpt } = node
+          return (
+            <div key={node.id}>
+              <Img fixed={frontmatter.image.childImageSharp.fixed} />
+              <h3>
+                <Link to={fields.url}>{frontmatter.title}</Link>
+              </h3>
+              <h5>
+                {frontmatter.milk_type}, {frontmatter.milk_treatment}
+              </h5>
+              <p>{excerpt}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
