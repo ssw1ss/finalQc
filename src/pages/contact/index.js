f1b979b8e05126c45fff17766e437b2ce8ebdf34
Lukas Hostettler <Lukas@mac.home>
2019-05-18 12:03:07 -0400

    added contact page

---
+++ src/pages/contact/index.js
@@ -0,0 +1,33 @@
+import React from "react"
+
+import { Layout } from "ui/layouts"
+import { Section } from "ui/base"
+
+const ContactPage = ({ data }) => (
+  <Layout>
+    <Section>
+      <form name="contact" method="POST" data-netlify="true">
+        <div>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Message: <textarea name="message" />
+          </label>
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </Section>
+  </Layout>
+)
+
+export default ContactPage
