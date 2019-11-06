b76fbb13d662d75367eca9b7a267248e5da92f09
Lukas Hostettler <Lukas@mac.local>
2019-11-06 11:36:57 -0500

    final working site

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/res/images/gatsby-icon.png` // This path is relative to the root of the site.
+        icon: `src/res/images/logo-icon.png` // This path is relative to the root of the site.
       }
     }
 
--- gatsby-node.js
+++ gatsby-node.js
@@ -45,58 +45,58 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   const ipp = 9 /* items per page */
 
-  const posts = new Promise((resolve, reject) => {
-    graphql(`
-      {
-        allMdx(filter: { frontmatter: { type: { eq: "post" } } }) {
-          totalCount
-          edges {
-            node {
-              id
-              fields {
-                url
-              }
-            }
-          }
-        }
-      }
-    `)
-      .then(result => {
-        // Create paginated blog (blog/2, blog/3 etc)
-        const totalCount = result.data.allMdx.totalCount
-        const numPages = Math.ceil(totalCount / ipp)
-        Array.from({ length: numPages }).forEach((_, i) => {
-          const currentPage = i + 1
-          createPage({
-            path:
-              i === 0
-                ? `${BLOG_PATH_PREFIX}`
-                : `${BLOG_PATH_PREFIX}/page/${currentPage}`,
-            component: path.resolve(`./src/templates/posts/index.js`),
-            context: {
-              limit: ipp,
-              skip: i * ipp,
-              numPages,
-              totalCount
-            }
-          })
-        })
+  // const posts = new Promise((resolve, reject) => {
+  //   graphql(`
+  //     {
+  //       allMdx(filter: { frontmatter: { type: { eq: "post" } } }) {
+  //         totalCount
+  //         edges {
+  //           node {
+  //             id
+  //             fields {
+  //               url
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `)
+  //     .then(result => {
+  //       // Create paginated blog (blog/2, blog/3 etc)
+  //       const totalCount = result.data.allMdx.totalCount
+  //       const numPages = Math.ceil(totalCount / ipp)
+  //       Array.from({ length: numPages }).forEach((_, i) => {
+  //         const currentPage = i + 1
+  //         createPage({
+  //           path:
+  //             i === 0
+  //               ? `${BLOG_PATH_PREFIX}`
+  //               : `${BLOG_PATH_PREFIX}/page/${currentPage}`,
+  //           component: path.resolve(`./src/templates/posts/index.js`),
+  //           context: {
+  //             limit: ipp,
+  //             skip: i * ipp,
+  //             numPages,
+  //             totalCount
+  //           }
+  //         })
+  //       })
 
-        // Create pages for each individual post
-        result.data.allMdx.edges.forEach(({ node }) => {
-          createPage({
-            path: `${node.fields.url}`,
-            component: path.resolve(`./src/templates/posts/post-single.js`),
-            context: { id: node.id }
-          })
-        })
-        resolve()
-      })
-      .catch(err => {
-        console.log("GN error")
-        reject(err)
-      })
-  })
+  //       // Create pages for each individual post
+  //       result.data.allMdx.edges.forEach(({ node }) => {
+  //         createPage({
+  //           path: `${node.fields.url}`,
+  //           component: path.resolve(`./src/templates/posts/post-single.js`),
+  //           context: { id: node.id }
+  //         })
+  //       })
+  //       resolve()
+  //     })
+  //     .catch(err => {
+  //       console.log("GN error")
+  //       reject(err)
+  //     })
+  // })
   const products = new Promise((resolve, reject) => {
     graphql(`
       {
