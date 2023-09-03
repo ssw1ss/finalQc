e2e3b982019bae9499baf503aaf7515e11cd74ef
Lukas <lukas1569@gmail.com>
2023-09-03 12:56:28 -0400

    fixes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,26 +4,11 @@ import Head from "next/head";
 
 const Layout = ({ children }: { children: any }) => {
   return (
-    <>
-      <Head>
-        <title>Quality Cheese</title>
-        <meta
-          name="description"
-          content="Quality Cheese, the finest cheeses from Switzerland"
-        />
-        <link rel="icon" href="/favicon.ico" />
-        <html lang="en" />
-        <link
-          href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,600,700"
-          rel="stylesheet"
-        />
-      </Head>
-      <div className="site__wrapper">
-        <Header />
-        <div>{children}</div>
-        <Footer />
-      </div>
-    </>
+    <div className="site__wrapper">
+      <Header />
+      <div>{children}</div>
+      <Footer />
+    </div>
   );
 };
 
