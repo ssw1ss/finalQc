b2ce94fc6374cce93361902c0a98f941d4eb871c
Lukas <lukas1569@gmail.com>
2023-09-03 22:22:30 -0400

    fix email

--- utils/links.ts
+++ utils/links.ts
@@ -3,5 +3,5 @@ export const links = [
   { label: "About", url: "/about" },
   { label: "Products", url: "/products" },
   { label: "Health", url: "/health" },
-  { label: "Contact", url: "mailto: qualitycheese@comcast.net" },
+  { label: "Contact", url: "mailto: mail@qualitycheese.net" },
 ];
